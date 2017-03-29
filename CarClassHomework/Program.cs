b8085ac4c1988b277ffa8e219bcb680cf3ec6b8d
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-03-28 19:20:53 -0500

    Properties on the Car class

--- CarClassHomework/Program.cs
+++ CarClassHomework/Program.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace CarClassHomework
 {
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            Car oldCar = new Car();
+
+
+            myCar.Make = "Hyundai";
+            myCar.Model = "Tiburon";
+            myCar.Year = 2008;
+            myCar.Color = "Black";
+
+
+            oldCar.Make = "Chevrolet";
+            oldCar.Model = "Trail Blazer";
+            oldCar.Year = 2004;
+            oldCar.Color = "White";
+
+
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year} {myCar.Color}");
+            Console.WriteLine($"{oldCar.Make} {oldCar.Model} {oldCar.Year} {oldCar.Color}");
         }
+ 
     }
 }
